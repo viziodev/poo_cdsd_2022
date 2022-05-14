540c6aa1107cea9869533bae4caa0eb914d25759
viziodev <viziodev03@gmail.com>
2022-05-14 19:14:48 +0000

    Request,Router,views

---
+++ controllers/ModuleController.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Core\Request;
+
+class ModuleController extends Controller{
+
+    
+    public function lister(){
+
+    }
+
+    public function ajouter(){
+        
+    }
+
+
+   
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,49 @@
 <?php
-
+/**
+ *  url:  localhost:8000/uri 
+ * uri => uniform ressource identfier => controller/use case
+ * controller => classe
+ *      use case   uri
+ *  --  ajouter => POST  localhost:8000/classe-add    
+ *   -- modifier => POST localhost:8000/classe-up
+ * --   lister=> GET     localhost:8000/classe
+ *   -- supprimer =>  GET    localhost:8000/classe-del
+ * Controller => cours
+ *      use case
+ *  --  ajouter => cours-add
+ *   -- modifier => cours-up
+ * --   lister=> cours
+ *   -- supprimer => cours-del
+ */
 
 
 require_once("../vendor/autoload.php");
+require_once('./../core/constantes.php');
+require_once('./../core/fonctions.php');
 
+use App\Controllers\SecuriteController;
+use App\Controllers\ClasseController;
+use App\Core\Router;
+use App\Exceptions\RouteNotFoundException;
+
+$router=new Router();
+
+//Enregister une route  : uri associe a un Controller et une action
+
+//$router->route("uri",[controller,action]);
+
+$router->route("/",[SecuriteController::class,"connexion"]);
+$router->route("/logout",[SecuriteController::class,"deconnexion"]);
+$router->route("/classe",[ClasseController::class,"lister"]);
+$router->route("/classe-add",[ClasseController::class,"ajouter"]);
+$router->route("/classe-up",[ClasseController::class,"modifier"]);
+$router->route("/classe-del",[ClasseController::class,"supprimer"]);
+
+try {
+  $router->resolve();
+} catch (RouteNotFoundException $ex) {
+   die($ex->message);
+}
 
 
 // Une variable d'une classe
@@ -34,12 +74,16 @@ require_once("../vendor/autoload.php");
 
 /* use App\Models\Module;
 use App\Models\RP;
+use App\Core\Request;
 use App\Core\DataBase;
 use App\Exceptions\BdConnexionException;
+use App\Controllers\ClasseController;
+
+
 
 
 $rp =new RP(); */
-use App\Models\RP;
+/* use App\Models\RP;
 $rp =new RP();
 $rp->setLogin("rp3");
 $rp->setPassword("rp");
@@ -47,7 +91,7 @@ $rp->insert();
 echo "<pre>";
 RP::selectAll();
 var_dump(RP::selectById(1));
-echo "</pre>";
+echo "</pre>"; */
 
 
   
